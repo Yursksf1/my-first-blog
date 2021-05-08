83246eeec5e22a129a4fd358e8046dedb6812356
Yurley Sanchez <yurs.ksf1@gmail.com>
2021-05-07 19:41:53 -0500

    add cbv

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
@@ -6,5 +8,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    # path('about', views.about, name='about'),
+    path('about/', TemplateView.as_view(template_name="blog/about.html")),
 
 ]
